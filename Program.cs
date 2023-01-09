e981a582bf53e54e7958a818058d5a5e382e328b
Dmitriy Kalininskiy <kalininskiyds@gmail.com>
2023-01-09 18:26:02 +0300

    55

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
 Например: 
 1 4 7 2     8 4 2 4 
 5 9 2 3  -> 5 9 2 3
-8 4 2 4     1 4 7 2  */
+8 4 2 4     1 4 7 2  
 
 int[,] array = new int[4, 4];
 for (int i = 0; i < array.GetLength(0); i++)
@@ -20,6 +20,43 @@ int x = array.GetLength(0) - 1; // от длины минусуем 1 ,что б
 for (int i = 0; i < array.GetLength(1); i++)
 {
     (array[0, i], array[x, i]) = (array[x, i], array[0, i]);
+}
+Console.WriteLine();
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}
+*/
+
+/* Задача 55. Задайте двумерный массив. Напишите программу, которая меняет стобцы и строки местами 
+Если это невозможно. пользователю должна выводится соответсвующая надпись.
+*/
+int[,] array = new int[4, 4];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(10);
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}
+if (array.GetLength(0) != array.GetLength(1))
+{
+    Console.Write($"Смена данного массива невозможна");
+    return;
+}
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = i + 1; j < array.GetLength(1); j++)
+    {
+
+        (array[i, j], array[j, i]) = (array[j, i], array[i, j]);
+    }
 }
 Console.WriteLine();
 for (int i = 0; i < array.GetLength(0); i++)
