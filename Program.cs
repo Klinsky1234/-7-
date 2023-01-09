9dad559fe7f9b5442863dec4c36d207214fc97c5
Dmitriy Kalininskiy <kalininskiyds@gmail.com>
2023-01-09 16:47:49 +0300

    53

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+/* Задача 53. Задайте двумерный массив. Напишите программу, которая меняет первую строчку и последнюю.
+Например: 
+1 4 7 2     8 4 2 4 
+5 9 2 3  -> 5 9 2 3
+8 4 2 4     1 4 7 2  */
+
+int[,] array = new int[3, 4];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(10);
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}
+for (int i = 0; i < array.GetLength(1); i++) // i в данном случае у нас счётчик и мы проходим по столбцам
+{
+    (array[0, i], array[2, i]) = (array[2, i], array[0, i]);
+}
+Console.WriteLine();
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+
+}
